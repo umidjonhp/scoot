5269d0fe5400081cc78e9f88da73ddc229d46b44
umidjonhp <umidjonmamasharifov@gmail.com>
2023-12-08 02:16:34 +0500

    Everything up-to-date

--- src/components/pages/home-components/information/Information.jsx
+++ src/components/pages/home-components/information/Information.jsx
@@ -1,5 +1,47 @@
+// import Img
+import card1Img from "../../../../../public/mock-images/home/information/app.svg"
+import card2Img from "../../../../../public/mock-images/home/information/scuter.svg"
+import card3Img from "../../../../../public/mock-images/home/information/scuter1.svg"
+
+const cardes = [
+  {
+    id: "1",
+    title: "Locate with app",
+    info: "Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away.",
+    photo: card1Img,
+  },
+  {
+    id: "2",
+    title: "Pick your scooter",
+    info: "We show the most important info for the scooters closest to you. So you know how much charge they have left and can see roughly how much it will cost.",
+    photo: card2Img,
+  },
+  {
+    id: "3",
+    title: "Enjoy the ride",
+    info: "Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet, and you’re off! Always lock bikes away from walkways and accessibility ramps.",
+    photo: card3Img,
+  },
+]
 export const Information = () => {
   return (
-    <div>Information</div>
+    <>
+      <div className="container mt-[160px] mb-[200px]">
+        <div className="flex items-center gap-[30px] relative">
+          <span className="absolute bg-lightGrey h-[10px] w-[974px] top-[15%] -left-[18%]"></span>
+          {
+            cardes.map((card) => {
+              return (
+                <div className="flex flex-col items-start" key={card.id}>
+                  <img src={card.photo} className="w-24 h-24 z-10" />
+                  <h4 className="text-darkNavy text-2xl font-bold font-spaceMono leading-7 mt-[40px]">{card.title}</h4>
+                  <p className="text-dimGrey text-[15px] font-normal font-lexendDeca leading-[25px] mt-[27px] ">{card.info}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
   )
 }
--- src/components/pages/home-components/main/Main.jsx
+++ src/components/pages/home-components/main/Main.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
-
+// import Img
+import boy from "../../../../../public/mock-images/home/homeMain/boy.png"
+import city from "../../../../../public/mock-images/home/homeMain/city.png"
+import money from "../../../../../public/mock-images/home/homeMain/money.png"
+import { Button } from "../../../common/button/Button";
+// data
+const services = [
+  {
+    id: "1",
+    title: "Easy to use riding telemetry",
+    info: "The Scoot app is available with riding telemetry. This means it can show you your average speed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.",
+    photo: boy,
+    state: true,
+  },
+  {
+    id: "2",
+    title: "Coming to a city near you",
+    info: "Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.",
+    photo: city,
+    state: false,
+  },
+  {
+    id: "3",
+    title: "Zero hassle payments",
+    info: "Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month.",
+    photo: money,
+    state: true,
+  },
+];
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className="container flex flex-col gap-40 mb-[200px]">
+      {
+        services.map((service) => {
+          return (
+            <div className={`flex items-center gap-[220px] ${service.state ? 'flex-row' : 'flex-row-reverse'}`}>
+              <div className="flex flex-col gap-6 items-start w-[47%]">
+                <h2 className="text-darkNavy text-5xl font-bold font-spaceMono leading-[48px]">{service.title}</h2>
+                <p className="text-dimGrey text-[15px] font-lexendDeca leading-[25px] mb-[16px] pr-[24px]">{service.info}</p>
+                <Button>Learn More</Button>
+              </div>
+              <div className="w-[445px] h-[445px] bg-zinc-300 rounded-full overflow-hidden">
+                <img src={service.photo} alt="" className="w-[100%] h-[100%]" />
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
